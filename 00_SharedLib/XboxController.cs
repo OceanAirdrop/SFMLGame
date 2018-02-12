72bb4f79d70f4d3e32f1bf827f45e51b1c5bd5f2
OceanAirdrop <OceanAirdrop@gmail.com>
2018-02-12 21:16:09 +0000

    joystic now controls square!

--- 00_SharedLib/XboxController.cs
+++ 00_SharedLib/XboxController.cs
@@ -12,7 +12,7 @@ namespace OceanAirdrop.SharedLib
 {
     public class XboxController
     {
-        public static CurrentButtonState JoyState = CurrentButtonState.None;
+        public static ControllerState JoyState = ControllerState.None;
 
         public static string RefreshButtonPressed()
         {
@@ -22,87 +22,87 @@ namespace OceanAirdrop.SharedLib
 
             if (Joystick.IsButtonPressed(0, (int)AvailableButton.A))
             {
-                SetJoystikFlag(CurrentButtonState.A_PRESSED);
+                SetJoystikFlag(ControllerState.A_PRESSED);
                 helperText = "Button Pressed: A";
             }
             else
-                ClearJoystikFlag(CurrentButtonState.A_PRESSED);
+                ClearJoystikFlag(ControllerState.A_PRESSED);
 
             // Check for buttons
             if (Joystick.IsButtonPressed(0, (int)AvailableButton.B))
             {
-                SetJoystikFlag(CurrentButtonState.B_PRESSED);
+                SetJoystikFlag(ControllerState.B_PRESSED);
                 helperText = "Button Pressed: B";
             }
             else
-                ClearJoystikFlag(CurrentButtonState.B_PRESSED);
+                ClearJoystikFlag(ControllerState.B_PRESSED);
 
 
             // Check for buttons
             if (Joystick.IsButtonPressed(0, (int)AvailableButton.X))
             {
-                SetJoystikFlag(CurrentButtonState.X_PRESSED);
+                SetJoystikFlag(ControllerState.X_PRESSED);
                 helperText = "Button Pressed: X";
             }
             else
-                ClearJoystikFlag(CurrentButtonState.X_PRESSED);
+                ClearJoystikFlag(ControllerState.X_PRESSED);
 
             // Check for buttons
             if (Joystick.IsButtonPressed(0, (int)AvailableButton.Y))
             {
-                SetJoystikFlag(CurrentButtonState.Y_PRESSED);
+                SetJoystikFlag(ControllerState.Y_PRESSED);
                 helperText = "Button Pressed: Y";
             }
             else
-                ClearJoystikFlag(CurrentButtonState.Y_PRESSED);
+                ClearJoystikFlag(ControllerState.Y_PRESSED);
 
             if (Joystick.IsButtonPressed(0, (int)AvailableButton.R_BUTT))
             {
-                SetJoystikFlag(CurrentButtonState.R_PRESSED);
+                SetJoystikFlag(ControllerState.R_PRESSED);
                 helperText = "Button Pressed: R_BUTT";
             }
             else
-                ClearJoystikFlag(CurrentButtonState.R_PRESSED);
+                ClearJoystikFlag(ControllerState.R_PRESSED);
 
             if (Joystick.IsButtonPressed(0, (int)AvailableButton.L_BUTT))
             {
-                SetJoystikFlag(CurrentButtonState.L_PRESSED);
+                SetJoystikFlag(ControllerState.L_PRESSED);
                 helperText = "Button Pressed: L_BUTT";
             }
             else
-                ClearJoystikFlag(CurrentButtonState.L_PRESSED);
+                ClearJoystikFlag(ControllerState.L_PRESSED);
 
             if (Joystick.IsButtonPressed(0, (int)AvailableButton.BACK))
             {
-                SetJoystikFlag(CurrentButtonState.BACK_PRESSED);
+                SetJoystikFlag(ControllerState.BACK_PRESSED);
                 helperText = "Button Pressed: BACK";
             }
             else
-                ClearJoystikFlag(CurrentButtonState.BACK_PRESSED);
+                ClearJoystikFlag(ControllerState.BACK_PRESSED);
 
             if (Joystick.IsButtonPressed(0, (int)AvailableButton.START))
             {
-                SetJoystikFlag(CurrentButtonState.START_PRESSED);
+                SetJoystikFlag(ControllerState.START_PRESSED);
                 helperText = "Button Pressed: START";
             }
             else
-                ClearJoystikFlag(CurrentButtonState.START_PRESSED);
+                ClearJoystikFlag(ControllerState.START_PRESSED);
 
             if (Joystick.IsButtonPressed(0, (int)AvailableButton.LEFT_THUMB_DOWN))
             {
-                SetJoystikFlag(CurrentButtonState.LEFT_THUMB_DOWN_PRESSED);
+                SetJoystikFlag(ControllerState.LEFT_THUMB_DOWN_PRESSED);
                 helperText = "Button Pressed: LEFT_THUMB_DOWN";
             }
             else
-                ClearJoystikFlag(CurrentButtonState.LEFT_THUMB_DOWN_PRESSED);
+                ClearJoystikFlag(ControllerState.LEFT_THUMB_DOWN_PRESSED);
 
             if (Joystick.IsButtonPressed(0, (int)AvailableButton.RIGHT_THUMB_DOWN))
             {
-                SetJoystikFlag(CurrentButtonState.RIGHT_THUMB_DOWN_PRESSED);
+                SetJoystikFlag(ControllerState.RIGHT_THUMB_DOWN_PRESSED);
                 helperText = "Button Pressed: RIGHT_THUMB_DOWN";
             }
             else
-                ClearJoystikFlag(CurrentButtonState.RIGHT_THUMB_DOWN_PRESSED);
+                ClearJoystikFlag(ControllerState.RIGHT_THUMB_DOWN_PRESSED);
 
             return helperText;
         }
@@ -121,55 +121,55 @@ namespace OceanAirdrop.SharedLib
             {
                 helperText = "no directional button pressed";
 
-                ClearJoystikFlag(CurrentButtonState.DPAD_LEFT_PRESSED);
-                ClearJoystikFlag(CurrentButtonState.DPAD_RIGHT_PRESSED);
-                ClearJoystikFlag(CurrentButtonState.DPAD_DOWN_PRESSED);
-                ClearJoystikFlag(CurrentButtonState.DPAD_UP_PRESSED);
+                ClearJoystikFlag(ControllerState.DPAD_LEFT_PRESSED);
+                ClearJoystikFlag(ControllerState.DPAD_RIGHT_PRESSED);
+                ClearJoystikFlag(ControllerState.DPAD_DOWN_PRESSED);
+                ClearJoystikFlag(ControllerState.DPAD_UP_PRESSED);
 
                 return helperText; // no button pressed!
             }
 
             if (joyY == -100)
             {
-                SetJoystikFlag(CurrentButtonState.DPAD_LEFT_PRESSED);
+                SetJoystikFlag(ControllerState.DPAD_LEFT_PRESSED);
                 helperText = string.Format("Axis Pressed: DPAD_LEFT {0}, {1}", joyX, joyY);
             }
             else
-                ClearJoystikFlag(CurrentButtonState.DPAD_LEFT_PRESSED);
+                ClearJoystikFlag(ControllerState.DPAD_LEFT_PRESSED);
 
             if (joyY == 100)
             {
-                SetJoystikFlag(CurrentButtonState.DPAD_RIGHT_PRESSED);
+                SetJoystikFlag(ControllerState.DPAD_RIGHT_PRESSED);
                 helperText = string.Format("Axis Pressed: DPAD_RIGHT {0}, {1}", joyX, joyY);
             }
             else
-                ClearJoystikFlag(CurrentButtonState.DPAD_RIGHT_PRESSED);
+                ClearJoystikFlag(ControllerState.DPAD_RIGHT_PRESSED);
 
             if (joyX == -100 || joyX == -70)
             {
-                SetJoystikFlag(CurrentButtonState.DPAD_DOWN_PRESSED);
+                SetJoystikFlag(ControllerState.DPAD_DOWN_PRESSED);
                 helperText = string.Format("Axis Pressed: DPAD_DOWN {0}, {1}", joyX, joyY);
             }
             else
-                ClearJoystikFlag(CurrentButtonState.DPAD_DOWN_PRESSED);
+                ClearJoystikFlag(ControllerState.DPAD_DOWN_PRESSED);
 
             if (joyX == 100)
             {
-                SetJoystikFlag(CurrentButtonState.DPAD_UP_PRESSED);
+                SetJoystikFlag(ControllerState.DPAD_UP_PRESSED);
                 helperText = string.Format("Axis Pressed: DPAD_UP {0}, {1}", joyX, joyY);
             }
             else
-                ClearJoystikFlag(CurrentButtonState.DPAD_UP_PRESSED);
+                ClearJoystikFlag(ControllerState.DPAD_UP_PRESSED);
 
             return helperText;
         }
 
-        private static void SetJoystikFlag(CurrentButtonState state)
+        private static void SetJoystikFlag(ControllerState state)
         {
             JoyState = EnumUtil.SetFlag(JoyState, state);
         }
 
-        private static void ClearJoystikFlag(CurrentButtonState state)
+        private static void ClearJoystikFlag(ControllerState state)
         {
             JoyState = EnumUtil.UnsetFlag(JoyState, state);
         }
@@ -178,40 +178,40 @@ namespace OceanAirdrop.SharedLib
 
         enum AvailableButton
         {
-            A = 0,
-            B = 1,
-            X = 2,
-            Y = 3,
-            L_BUTT = 4,
-            R_BUTT = 5,
-            BACK = 6,
-            START = 7,
-            LEFT_THUMB_DOWN = 8,
+            A                = 0,
+            B                = 1,
+            X                = 2,
+            Y                = 3,
+            L_BUTT           = 4,
+            R_BUTT           = 5,
+            BACK             = 6,
+            START            = 7,
+            LEFT_THUMB_DOWN  = 8,
             RIGHT_THUMB_DOWN = 9,
-            DPAD_UP = 20,
-            DPAD_DOWN = 21,
-            DPAD_LEFT = 22,
-            DPAD_RIGHT = 23
+            DPAD_UP          = 20,
+            DPAD_DOWN        = 21,
+            DPAD_LEFT        = 22,
+            DPAD_RIGHT       = 23
         };
 
         [Flags]
-        public enum CurrentButtonState
+        public enum ControllerState
         {
-            None = 0,
-            A_PRESSED = 1,
-            B_PRESSED = 2,
-            X_PRESSED = 4,
-            Y_PRESSED = 8,
-            L_PRESSED = 16,
-            R_PRESSED = 32,
-            BACK_PRESSED = 64,
-            START_PRESSED = 128,
-            LEFT_THUMB_DOWN_PRESSED = 256,
+            None                     = 0,
+            A_PRESSED                = 1,
+            B_PRESSED                = 2,
+            X_PRESSED                = 4,
+            Y_PRESSED                = 8,
+            L_PRESSED                = 16,
+            R_PRESSED                = 32,
+            BACK_PRESSED             = 64,
+            START_PRESSED            = 128,
+            LEFT_THUMB_DOWN_PRESSED  = 256,
             RIGHT_THUMB_DOWN_PRESSED = 512,
-            DPAD_UP_PRESSED = 1024,
-            DPAD_DOWN_PRESSED = 2048,
-            DPAD_LEFT_PRESSED = 4096,
-            DPAD_RIGHT_PRESSED = 8192
+            DPAD_UP_PRESSED          = 1024,
+            DPAD_DOWN_PRESSED        = 2048,
+            DPAD_LEFT_PRESSED        = 4096,
+            DPAD_RIGHT_PRESSED       = 8192
         }
     }
 }
--- 02_JoystickInput/Program.cs
+++ 02_JoystickInput/Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static OceanAirdrop.SharedLib.XboxController;
 
 namespace _02_JoystickInput
 {
@@ -28,6 +29,12 @@ namespace _02_JoystickInput
             CircleShape shape = new CircleShape(10);
             shape.FillColor = new Color(Color.Red);
 
+            RectangleShape square = new RectangleShape();
+            square.FillColor    = new Color(Color.Red);
+            square.Position     = new Vector2f(app.Size.X / 2, app.Size.Y / 2);
+            square.OutlineColor = new Color(0, 0, 0, 255);
+            square.Size         = new Vector2f(50, 50);
+
             // Start the game loop
             while (app.IsOpen())
             {
@@ -38,9 +45,24 @@ namespace _02_JoystickInput
                 app.Clear(windowColor);
 
                 app.Draw(shape);
+                app.Draw(square);
 
                 Console.WriteLine(XboxController.RefreshButtonPressed());
                 Console.WriteLine(XboxController.RefreshAxisPressed());
+
+
+                if (JoyState.HasFlag(ControllerState.DPAD_UP_PRESSED))
+                    square.Position = new Vector2f(square.Position.X, square.Position.Y - 1);
+
+                if (JoyState.HasFlag(ControllerState.DPAD_DOWN_PRESSED))
+                    square.Position = new Vector2f(square.Position.X, square.Position.Y + 1);
+
+                if (JoyState.HasFlag(ControllerState.DPAD_LEFT_PRESSED))
+                    square.Position = new Vector2f(square.Position.X - 1, square.Position.Y);
+
+                if (JoyState.HasFlag(ControllerState.DPAD_RIGHT_PRESSED))
+                    square.Position = new Vector2f(square.Position.X + 1, square.Position.Y);
+
 
                 // Update the window
                 app.Display();
