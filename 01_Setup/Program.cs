20b6bf51d99920fa5e9c189bfefd7a313fd167e2
OceanAirdrop <OceanAirdrop@gmail.com>
2018-02-12 18:51:23 +0000

    My first SFML window

--- 01_Setup/Program.cs
+++ 01_Setup/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SFML.Graphics;
+using SFML.Window;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,32 @@ namespace OceanAirdrop.Setup
 {
     class Program
     {
+        static void OnClose(object sender, EventArgs e)
+        {
+            // Close the window when OnClose event is received
+            RenderWindow window = (RenderWindow)sender;
+            window.Close();
+        }
+
         static void Main(string[] args)
         {
+            RenderWindow app = new RenderWindow(new VideoMode(800, 600), "OMG, it works!");
+            app.Closed += new EventHandler(OnClose);
+
+            Color windowColor = new Color(77, 255, 130);
+
+            // Start the game loop
+            while (app.IsOpen())
+            {
+                // Process events
+                app.DispatchEvents();
+
+                // Clear screen
+                app.Clear(windowColor);
+
+                // Update the window
+                app.Display();
+            }
         }
     }
 }
